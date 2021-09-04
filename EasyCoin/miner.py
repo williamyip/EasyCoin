5d7e124bbfac8c9947fe82751e9398126d92de3b
williamyip <williamyip67@hotmail.com>
2021-09-05 03:46:03 +0800

    created validate_signature and welcome_msg functions

--- EasyCoin/miner.py
+++ EasyCoin/miner.py
@@ -223,3 +223,16 @@ def transaction():
         NODE_PENDING_TRANSACTIONS[:] = []
         return pending
 
+def validate_signature(public_key, signature, message):
+    """Verifies if the signature is correct. This is used to prove
+    it's you (and not someone else) trying to do a transaction with your
+    address. Called when a user tries to submit a new transaction.
+    """
+    public_key = (base64.b64decode(public_key)).hex()
+    signature = base64.b64decode(signature)
+    vk = ecdsa.VerifyingKey.from_string(bytes.fromhex(public_key), curve=ecdsa.SECP256k1)
+    # Try changing into an if/else statement as except is too broad.
+    try:
+        return vk.verify(signature, message.encode())
+    except:
+        return False
