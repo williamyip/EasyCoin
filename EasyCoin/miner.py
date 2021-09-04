50981aca3ffc4085f4b4a3609c09d85c1aa97e82
williamyip <williamyip67@hotmail.com>
2021-09-05 03:06:01 +0800

    created Block class

--- EasyCoin/miner.py
+++ EasyCoin/miner.py
@@ -10,3 +10,31 @@ import ecdsa
 from miner_config import MINER_ADDRESS, MINER_NODE_URL, PEER_NODES
 
 node = Flask(__name__)
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        """Returns a new Block object. Each block is "chained" to its previous
+        by calling its unique hash.
+        Args:
+            index (int): Block number.
+            timestamp (int): Block creation timestamp.
+            data (str): Data to be sent.
+            previous_hash(str): String representing previous block unique hash.
+        Attrib:
+            index (int): Block number.
+            timestamp (int): Block creation timestamp.
+            data (str): Data to be sent.
+            previous_hash(str): String representing previous block unique hash.
+            hash(str): Current block unique hash.
+        """
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.hash_block()
+
+    def hash_block(self):
+        """Creates the unique hash for the block. It uses sha256."""
+        sha = hashlib.sha256()
+        sha.update((str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)).encode('utf-8'))
+        return sha.hexdigest()
